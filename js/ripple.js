aeffd7c2931b51e71900816e9f70396b6458e25b
Herman Bergwerf <hermanbergwerf@gmail.com>
2015-06-01 14:56:37 +0200

    Add Material Design ripple effect

---
+++ js/ripple.js
@@ -0,0 +1,34 @@
+(function (window, $) {
+	$(function() {
+		$('.posts .post').addClass('ripple').data('ripple-color', '#bdbdbd');
+		$('.ripple').on('mousedown', function (event) {
+			event.preventDefault();
+
+			$('.ripple .ripple-effect').remove();
+
+			var $div = $('<div/>'),
+				btnOffset = $(this).offset(),
+				xPos = event.pageX - btnOffset.left,
+				yPos = event.pageY - btnOffset.top;
+
+			$div.addClass('ripple-effect');
+			var $ripple = $('.ripple-effect');
+
+			$ripple.css('height', $(this).height());
+			$ripple.css('width', $(this).height());
+			$div.css({
+				top: yPos - ($ripple.height()/2),
+				left: xPos - ($ripple.width()/2),
+				background: $(this).data('ripple-color')
+			}).prependTo($(this));
+		});
+		$('.ripple').on('mouseup mouseout', function (event) {
+			var $ripples = $('.ripple .ripple-effect');
+			$ripples.addClass('out');
+			window.setTimeout(function()
+			{
+				$ripples.remove();
+			}, 300)
+		});
+	});
+})(window, jQuery);
